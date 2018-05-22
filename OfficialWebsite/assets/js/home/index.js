684ad61a1e2e0a516ff3a8699cdfc4f11f6588db
黄丽雅 <418607528@qq.com>
2018-05-22 21:10:34 +0800

    新增了提供的服务板块

---
+++ OfficialWebsite/assets/js/home/index.js
@@ -0,0 +1,23 @@
+// .jf-home-services-icon
+$(function(){
+    autoIconfont();
+    // 如果屏幕高度宽度发生了变化，再调用
+    window.onresize = autoIconfont;
+    $('.jf-home-services-icon').mouseover(function () {
+        var height = $(".jf-home-services-wrap").css("height");
+        $(".jf-home-services-icon").css('line-height', height);
+        var height_string = (parseFloat(height)/2).toFixed(0) + "px";
+        $(".jf-home-services-icon").css('font-size', height_string);
+    });
+    $('.jf-home-services-icon').mouseleave(function () {
+        autoIconfont();
+    })
+    // 自动改变iconfont的行高
+    function autoIconfont() {
+        var height = $(".jf-home-services-wrap").css("height");
+        $(".jf-home-services-icon").css('line-height', height);
+        var height_string = (parseFloat(height)/3).toFixed(0) + "px";
+        $(".jf-home-services-icon").css('font-size', height_string);
+    };
+
+});
