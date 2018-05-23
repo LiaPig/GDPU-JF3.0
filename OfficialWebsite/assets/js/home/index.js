0129b2562332252271296140f4947d9fc91315a4
黄丽雅 <418607528@qq.com>
2018-05-23 11:42:42 +0800

    完成了服务模块

--- OfficialWebsite/assets/js/home/index.js
+++ OfficialWebsite/assets/js/home/index.js
@@ -5,12 +5,15 @@ $(function(){
     window.onresize = autoIconfont;
     $('.jf-home-services-icon').mouseover(function () {
         var height = $(".jf-home-services-wrap").css("height");
-        $(".jf-home-services-icon").css('line-height', height);
+        $(this).css('line-height', height);
         var height_string = (parseFloat(height)/2).toFixed(0) + "px";
-        $(".jf-home-services-icon").css('font-size', height_string);
+        $(this).css('font-size', height_string);
     });
     $('.jf-home-services-icon').mouseleave(function () {
-        autoIconfont();
+        var height = $(".jf-home-services-wrap").css("height");
+        $(this).css('line-height', height);
+        var height_string = (parseFloat(height)/3).toFixed(0) + "px";
+        $(this).css('font-size', height_string);
     })
     // 自动改变iconfont的行高
     function autoIconfont() {
