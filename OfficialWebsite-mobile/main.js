7808f10b9e040e17bd22f98e1807e7f79bb62f93
黄丽雅 <418607528@qq.com>
2018-06-07 15:26:06 +0800

    1.更换了顶部的图片，去掉了有字库的引入 2.初步完成了头部菜单

--- OfficialWebsite-mobile/main.js
+++ OfficialWebsite-mobile/main.js
@@ -3,3 +3,17 @@ import 'normalize.css'
 import 'mobi.css'
 import './assets/css/common.scss'
 import './assets/js/lib/swiper-4.2.2.min.js'
+import $ from './assets/js/lib/jquery.min'
+
+$(document).ready(function(){
+    $('.jf-header-menu-more').click(function(){
+        $('.jf-header-popper').slideDown('slow');
+        $(this).hide();
+        $('.jf-header-menu-close').show();
+    });
+    $('.jf-header-menu-close').click(function(){
+        $('.jf-header-popper').slideUp('slow');
+        $(this).hide();
+        $('.jf-header-menu-more').show();
+    });
+});
