87dbeb8d150d2e29fb2c029e1f1aafbfe1d78445
黄丽雅 <418607528@qq.com>
2018-06-20 11:27:43 +0800

    引入了normallize.css

--- cms/src/App.vue
+++ cms/src/App.vue
@@ -11,13 +11,15 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  $jf-blue: #45aed6;
+  $jf-black-bg: #2f3238;
+  $jf-black-color: #727b8b;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
--- cms/src/main.js
+++ cms/src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vuex from 'vuex'
+// import Vuex from 'vuex'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import 'normalize.css'
 
-Vue.use(Vuex)
+// Vue.use(Vuex)
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
