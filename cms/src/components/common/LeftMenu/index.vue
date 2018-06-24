f7f607961946e881723f07c9f714dfd1afefaf12
Lia <418607528@qq.com>
2018-06-24 18:45:58 +0800

    增了了当前的active

--- cms/src/components/common/LeftMenu/index.vue
+++ cms/src/components/common/LeftMenu/index.vue
@@ -4,7 +4,7 @@
       <el-menu
         router
         :collapse="false"
-        class="el-menu-vertical-demo"
+        :default-active="1"
         background-color="#2f3238"
         text-color="#fff"
         active-text-color="#45aed6"
