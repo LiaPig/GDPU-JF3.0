8905f404f5ffdab5ee25940c695acf77f129e350
黄丽雅 <418607528@qq.com>
2018-06-25 17:51:14 +0800

    初步加入路由守卫

--- cms/src/router/index.js
+++ cms/src/router/index.js
@@ -24,4 +24,24 @@ const router = new Router({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(r => r.meta.requiresAuth)) {  // 判断该路由是否需要登录权限
+    // console.log(store.getters.isLogin);
+    // if (store.getters.isLogin) {  // 通过vuex 如果当前有登录
+    //   next();
+    // }
+    // else {
+    //   console.log("没有登录吖")
+    //   next({
+    //     path: '/login',
+    //     query: {redirect: to.fullPath}
+    //   })
+    // }
+    }
+    else {
+      next();
+    }
+    console.log(to.name, to.path)
+  })
+
 export default router
